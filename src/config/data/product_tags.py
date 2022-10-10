ef7b665dc7ad15ea7d9e7f9a86597aa69d2acf1a
Ricardo-we <totgamer75@gmail.com>
2022-10-10 17:58:58 -0600

    Added config data

---
+++ src/config/data/product_tags.py
@@ -0,0 +1,26 @@
+product_tags_data = [
+    {
+        "name": "mesas",
+        "description": "Mesas de todas maneras"
+    },
+    {
+        "name": "",
+        "description": "Mesas de todas maneras"
+    },
+    {
+        "name": "mesas",
+        "description": "Mesas de todas maneras"
+    },
+    {
+        "name": "mesas",
+        "description": "Mesas de todas maneras"
+    },
+    {
+        "name": "mesas",
+        "description": "Mesas de todas maneras"
+    },
+    {
+        "name": "mesas",
+        "description": "Mesas de todas maneras"
+    },
+]
